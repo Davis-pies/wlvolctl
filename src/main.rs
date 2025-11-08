8409045782589c4812d9fe4044ed69c2a4e71ea5
Davis Pies <Davis.Pies1@gmail.com>
2025-11-07 23:27:50 -0800

    Kinda working gtk4

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,24 @@ mod pulseaudio_cli;
 mod ui;
 
 fn main() {
-    ui::run_ui();
-}
+    let args: Vec<String> = std::env::args().collect();
+    println!("DEBUG: raw args = {:?}", args);
 
+    if args.len() > 1 {
+        println!("DEBUG: first argument = {}", args[1]);
+        match args[1].as_str() {
+            "--popup" => {
+                println!("DEBUG: entering popup mode");
+                ui::run_popup_ui();
+            }
+            _ => {
+                eprintln!("Unknown option {}", args[1]);
+                std::process::exit(1);
+            }
+        }
+    } else {
+        println!("DEBUG: entering full mode");
+        ui::run_full_ui();
+    }
+}
 
